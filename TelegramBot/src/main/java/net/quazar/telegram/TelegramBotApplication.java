2cd600a64aee1f3d02d1ed59eb60fdc60ffc8b3c
SQuazar <flawesv@yandex.ru>
2023-05-17 02:16:39 +0300

    Some changes

--- TelegramBot/src/main/java/net/quazar/telegram/TelegramBotApplication.java
+++ TelegramBot/src/main/java/net/quazar/telegram/TelegramBotApplication.java
@@ -2,11 +2,10 @@ package net.quazar.telegram;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
-@SpringBootApplication(exclude = UserDetailsServiceAutoConfiguration.class)
+@SpringBootApplication
 @EnableFeignClients("net.quazar.telegram.proxy")
 @EnableDiscoveryClient
 public class TelegramBotApplication {
