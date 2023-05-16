18cfc2320be591c32c18c38c149407d420e95ee0
SQuazar <flawesv@yandex.ru>
2023-05-17 02:28:05 +0300

    Added TelegramBotServerProxy

---
+++ ApiGateway/src/main/java/net/quazar/apigateway/proxy/TelegramBotServerProxy.java
@@ -0,0 +1,25 @@
+package net.quazar.apigateway.proxy;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Data;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@FeignClient("telegram-bot")
+public interface TelegramBotServerProxy {
+    @PostMapping("/sendAnnouncements")
+    void startSendingAnnouncements(@RequestBody SendAnnouncementsRequest request);
+
+    @Data
+    final class SendAnnouncementsRequest {
+        private final String text;
+        @JsonProperty(value = "category_id", defaultValue = "0")
+        private final int categoryId;
+        @JsonProperty("roles")
+        private final Set<Integer> rolesIds = new HashSet<>();
+    }
+}
