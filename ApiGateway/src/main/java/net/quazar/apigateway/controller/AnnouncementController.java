c32e74e94917e147ab0403879daff5e8499b6030
SQuazar <flawesv@yandex.ru>
2023-05-17 02:29:11 +0300

    Created AnnouncementController

---
+++ ApiGateway/src/main/java/net/quazar/apigateway/controller/AnnouncementController.java
@@ -0,0 +1,24 @@
+package net.quazar.apigateway.controller;
+
+import lombok.AllArgsConstructor;
+import net.quazar.apigateway.auth.entity.AnnouncementDto;
+import net.quazar.apigateway.proxy.ResourceServerProxy;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@AllArgsConstructor
+@RestController
+@RequestMapping("/api/announcements")
+public class AnnouncementController {
+    private final ResourceServerProxy resourceServerProxy;
+
+    @PreAuthorize("hasAuthority('resource.announcement.save')")
+    @PostMapping
+    public ResponseEntity<AnnouncementDto> save(@RequestParam String text) {
+        return resourceServerProxy.saveAnnouncement(text);
+    }
+}
