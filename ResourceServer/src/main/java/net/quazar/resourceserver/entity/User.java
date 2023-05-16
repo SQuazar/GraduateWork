1c7d01a6c09eab8f9038a91708c236cd543eae28
SQuazar <flawesv@yandex.ru>
2023-05-17 02:21:29 +0300

    Added User class

---
+++ ResourceServer/src/main/java/net/quazar/resourceserver/entity/User.java
@@ -0,0 +1,35 @@
+package net.quazar.resourceserver.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Entity
+@Table(name = "users")
+public class User {
+    @EqualsAndHashCode.Include
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private @Id Integer id;
+
+    private @Column(nullable = false, unique = true) String username;
+    private @Column(nullable = false) String passwordHash;
+
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(name = "user_roles",
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id"))
+    private Set<Role> roles = new HashSet<>();
+
+    @ElementCollection(fetch = FetchType.EAGER)
+    @CollectionTable(name = "user_permissions",
+            joinColumns = @JoinColumn(name = "user_id", referencedColumnName = "id"))
+    @Column(name = "permission")
+    private Set<String> permissions = new HashSet<>();
+}
