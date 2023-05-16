15a8c98c8cfd43f68d75e387327a36a97273cb67
SQuazar <flawesv@yandex.ru>
2023-05-17 02:23:23 +0300

    Added AnnouncementRepository

---
+++ ResourceServer/src/main/java/net/quazar/resourceserver/repository/AnnouncementRepository.java
@@ -0,0 +1,9 @@
+package net.quazar.resourceserver.repository;
+
+import net.quazar.resourceserver.entity.Announcement;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AnnouncementRepository extends JpaRepository<Announcement, Integer> {
+}
