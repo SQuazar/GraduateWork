6471deb47f16b7a421bb4c1f444ce615f8020e84
SQuazar <flawesv@yandex.ru>
2023-05-17 02:28:35 +0300

    Excludes UserDetailsServiceAutoconfiguration from SpringApplication

--- ApiGateway/src/main/java/net/quazar/apigateway/ApiGatewayApplication.java
+++ ApiGateway/src/main/java/net/quazar/apigateway/ApiGatewayApplication.java
@@ -2,10 +2,11 @@ package net.quazar.apigateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @EnableFeignClients("net.quazar.apigateway.proxy")
-@SpringBootApplication
+@SpringBootApplication(exclude = UserDetailsServiceAutoConfiguration.class)
 public class ApiGatewayApplication {
 
     public static void main(String[] args) {
