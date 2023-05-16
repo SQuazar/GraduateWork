8e115371d63fc966f19ca7eccec19a145063f326
SQuazar <flawesv@yandex.ru>
2023-05-17 02:23:12 +0300

    Added RoleRepository

---
+++ ResourceServer/src/main/java/net/quazar/resourceserver/repository/RoleRepository.java
@@ -0,0 +1,9 @@
+package net.quazar.resourceserver.repository;
+
+import net.quazar.resourceserver.entity.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RoleRepository extends JpaRepository<Role, Integer> {
+}
