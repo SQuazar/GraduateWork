4a2479f97f72d18d39827fe941003f177c40ec4f
SQuazar <flawesv@yandex.ru>
2023-05-17 02:18:07 +0300

    Added bot state handler registry

---
+++ TelegramBot/src/main/java/net/quazar/telegram/bot/handler/AnnouncementBotStateHandlerRegistry.java
@@ -0,0 +1,33 @@
+package net.quazar.telegram.bot.handler;
+
+import org.springframework.lang.Nullable;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ConcurrentHashMap;
+
+public class AnnouncementBotStateHandlerRegistry implements StateHandlerRegistry {
+    private final ConcurrentHashMap<Integer, List<StateHandler>> handlersMap = new ConcurrentHashMap<>();
+
+    @Override
+    public void register(int state, StateHandler handler) {
+        List<StateHandler> handlers = this.handlersMap.get(state);
+        if (handlers != null) {
+            handlers.add(handler);
+            return;
+        }
+        handlers = new ArrayList<>();
+        handlers.add(handler);
+        this.handlersMap.put(state, handlers);
+    }
+
+    @Override
+    public void unregister(int state) {
+        this.handlersMap.remove(state);
+    }
+
+    @Override
+    public @Nullable List<StateHandler> getStateHandlers(int state) {
+        return this.handlersMap.get(state);
+    }
+}
