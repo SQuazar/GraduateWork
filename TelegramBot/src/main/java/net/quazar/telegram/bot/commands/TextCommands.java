33c510b98d8a114257a7a84f4ffda61742acd0c9
SQuazar <flawesv@yandex.ru>
2023-05-17 02:15:39 +0300

    Added text commands constants class

---
+++ TelegramBot/src/main/java/net/quazar/telegram/bot/commands/TextCommands.java
@@ -0,0 +1,32 @@
+package net.quazar.telegram.bot.commands;
+
+public final class TextCommands {
+    public static final class StartCommands {
+        public static final String SUBSCRIBE = "Подписаться";
+    }
+
+    public static final class MenuCommands {
+        public static final String MY_SUBSCRIPTION = "Моя подписка";
+    }
+
+    public static final class MySubscriptionCommands {
+        public static final String CATEGORIES = "Категории";
+        public static final String UNSUBSCRIBE = "Отписаться";
+    }
+
+    public static final class MyCategoriesCommands {
+        public static final String SUBSCRIBE_CATEGORY = "Подписаться на категорию";
+        public static final String UNSUBSCRIBE_CATEGORY = "Отписаться от категории";
+        public static final String GO_BACK = "Назад";
+    }
+
+    public static final class AddCategoryCommands {
+        public static final String DONE = "Готово";
+    }
+
+    public static final class RemoveCategoryCommands {
+        public static final String DONE = "Готово";
+    }
+
+    public static final String BACK_TO_MENU = "Назад в меню";
+}
