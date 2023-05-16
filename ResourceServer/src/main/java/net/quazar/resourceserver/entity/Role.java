190f8ac0e452060064f75028d566fa20afe1ce04
SQuazar <flawesv@yandex.ru>
2023-05-17 02:21:19 +0300

    Added Role class

---
+++ ResourceServer/src/main/java/net/quazar/resourceserver/entity/Role.java
@@ -0,0 +1,26 @@
+package net.quazar.resourceserver.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.Set;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Entity
+@Table(name = "roles")
+public class Role {
+    @EqualsAndHashCode.Include
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private @Id Integer id;
+
+    private @Column(nullable = false, unique = true) String name;
+
+    @ElementCollection(fetch = FetchType.EAGER)
+    @CollectionTable(name = "role_permissions",
+            joinColumns = @JoinColumn(name = "role_id", referencedColumnName = "id"))
+    private @Column(name = "permission") Set<String> permissions;
+}
