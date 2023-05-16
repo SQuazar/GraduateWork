782c4b6630aa794097e64fe2053fa7c85cf2935f
SQuazar <flawesv@yandex.ru>
2023-05-17 02:16:13 +0300

    Added keyboards class

---
+++ TelegramBot/src/main/java/net/quazar/telegram/bot/Keyboards.java
@@ -0,0 +1,92 @@
+package net.quazar.telegram.bot;
+
+import net.quazar.telegram.bot.commands.TextCommands;
+import net.quazar.telegram.bot.handler.StateHandler;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.ReplyKeyboard;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.ReplyKeyboardMarkup;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.KeyboardButton;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.KeyboardRow;
+
+import java.util.List;
+
+public final class Keyboards {
+    public static ReplyKeyboard startKeyboard() {
+        return ReplyKeyboardMarkup.builder()
+                .keyboardRow(new KeyboardRow(List.of(
+                        KeyboardButton.builder()
+                                .text(TextCommands.StartCommands.SUBSCRIBE)
+                                .build()
+                )))
+                .resizeKeyboard(true)
+                .build();
+    }
+
+    public static ReplyKeyboard menuKeyboard() {
+        return ReplyKeyboardMarkup.builder()
+                .keyboard(List.of(
+                        new KeyboardRow(List.of(
+                                KeyboardButton.builder()
+                                        .text(TextCommands.MenuCommands.MY_SUBSCRIPTION)
+                                        .build()
+                        ))
+                ))
+                .resizeKeyboard(true)
+                .build();
+    }
+
+    public static ReplyKeyboard mySubscriptionKeyboard() {
+        return ReplyKeyboardMarkup.builder()
+                .keyboard(List.of(
+                        new KeyboardRow(List.of(
+                                KeyboardButton.builder()
+                                        .text(TextCommands.MySubscriptionCommands.CATEGORIES)
+                                        .build()
+                        )),
+                        new KeyboardRow(List.of(
+                                KeyboardButton.builder()
+                                        .text(TextCommands.MySubscriptionCommands.UNSUBSCRIBE)
+                                        .build()
+                        )),
+                        new KeyboardRow(List.of(
+                                KeyboardButton.builder()
+                                        .text(TextCommands.BACK_TO_MENU)
+                                        .build()
+                        ))
+                ))
+                .resizeKeyboard(true)
+                .build();
+    }
+
+    public static ReplyKeyboard categoriesEdit() {
+        return ReplyKeyboardMarkup.builder()
+                .keyboard(List.of(
+                        new KeyboardRow(List.of(
+                                KeyboardButton.builder()
+                                        .text(TextCommands.MyCategoriesCommands.SUBSCRIBE_CATEGORY)
+                                        .build()
+                        )),
+                        new KeyboardRow(List.of(
+                                KeyboardButton.builder()
+                                        .text(TextCommands.MyCategoriesCommands.UNSUBSCRIBE_CATEGORY)
+                                        .build()
+                        )),
+                        new KeyboardRow(List.of(
+                                KeyboardButton.builder()
+                                        .text(TextCommands.MyCategoriesCommands.GO_BACK)
+                                        .build()
+                        ))
+                ))
+                .resizeKeyboard(true)
+                .build();
+    }
+
+    public static ReplyKeyboard getKeyboardByState(int state) {
+        return switch (state) {
+            case StateHandler.State.START -> startKeyboard();
+            case StateHandler.State.MY_SUBSCRIPTION -> mySubscriptionKeyboard();
+            case StateHandler.State.CATEGORIES_EDIT, StateHandler.State.CATEGORY_SUBSCRIBE, StateHandler.State.CATEGORY_UNSUBSCRIBE ->
+                    categoriesEdit();
+            default -> menuKeyboard();
+        };
+    }
+}
