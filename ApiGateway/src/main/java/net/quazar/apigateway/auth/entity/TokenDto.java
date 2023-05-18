41aa280215d9720fd0e7ae8e7a14a3f692b18986
SQuazar <flawesv@yandex.ru>
2023-05-18 23:18:26 +0300

    Removed TokenDto

--- ApiGateway/src/main/java/net/quazar/apigateway/auth/entity/TokenDto.java
+++
@@ -1,8 +0,0 @@
-package net.quazar.apigateway.auth.entity;
-
-public record TokenDto(int id, String token, TokenType type, long expire, boolean revoked) {
-    public enum TokenType {
-        ACCESS,
-        REFRESH
-    }
-}
