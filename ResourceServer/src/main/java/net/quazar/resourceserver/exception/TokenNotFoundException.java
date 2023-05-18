80e347eee0916736eca9745269f7a0cd72efe877
SQuazar <flawesv@yandex.ru>
2023-05-18 23:10:48 +0300

    Added TokenNotFoundException

---
+++ ResourceServer/src/main/java/net/quazar/resourceserver/exception/TokenNotFoundException.java
@@ -0,0 +1,22 @@
+package net.quazar.resourceserver.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.NOT_FOUND)
+public class TokenNotFoundException extends RuntimeException {
+    public TokenNotFoundException() {
+    }
+
+    public TokenNotFoundException(String message) {
+        super(message);
+    }
+
+    public TokenNotFoundException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public TokenNotFoundException(Throwable cause) {
+        super(cause);
+    }
+}
