cbdfa64a3f41556f0caa6b98c5e832f32f98e025
SQuazar <flawesv@yandex.ru>
2023-05-18 23:22:15 +0300

    Filter upgrade. Is now authorities updates in real time. Also, you can revoke the token

--- ApiGateway/src/main/java/net/quazar/apigateway/config/filter/JwtAuthenticationFilter.java
+++ ApiGateway/src/main/java/net/quazar/apigateway/config/filter/JwtAuthenticationFilter.java
@@ -1,14 +1,21 @@
 package net.quazar.apigateway.config.filter;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.AllArgsConstructor;
 import net.quazar.apigateway.config.service.JwtService;
-import net.quazar.apigateway.proxy.AuthorizationServerProxy;
+import net.quazar.apigateway.entity.ApiError;
+import net.quazar.apigateway.exception.NotFoundException;
+import net.quazar.apigateway.exception.jwt.InvalidTokenException;
+import net.quazar.apigateway.exception.jwt.TokenRevokedException;
+import net.quazar.apigateway.proxy.ResourceServerProxy;
+import net.quazar.apigateway.proxy.resource.TokenResponse;
+import net.quazar.apigateway.proxy.resource.UserResponse;
+import org.springframework.lang.NonNull;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.User;
@@ -17,32 +24,67 @@ import org.springframework.security.web.authentication.WebAuthenticationDetailsS
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
-import java.util.Arrays;
-import java.util.List;
+import java.time.LocalDateTime;
 
 @AllArgsConstructor
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
     private final JwtService jwtService;
-    private final AuthorizationServerProxy authorizationServerProxy;
+    private final ResourceServerProxy resourceServerProxy;
+    private final ObjectMapper mapper = new ObjectMapper();
 
     @Override
-    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+    protected void doFilterInternal(HttpServletRequest request,
+                                    @NonNull HttpServletResponse response,
+                                    @NonNull FilterChain filterChain) throws ServletException, IOException {
         String authHeader = request.getHeader("Authorization");
         if (authHeader == null || !authHeader.startsWith("Bearer ")) {
             filterChain.doFilter(request, response);
             return;
         }
         String jwt = authHeader.substring("Bearer ".length());
-        authorizationServerProxy.verifyToken(authHeader);
+        TokenResponse token;
+        try {
+            token = resourceServerProxy.getToken(jwt);
+            if (!jwt.equals(token.token())) throw new InvalidTokenException("Invalid token", LocalDateTime.now().toString());
+            if (token.revoked()) throw new TokenRevokedException("Token is revoked", LocalDateTime.now().toString());
+        } catch (NotFoundException e) {
+            filterChain.doFilter(request, response);
+            return;
+        } catch (InvalidTokenException | TokenRevokedException e) {
+            ApiError error = ApiError.builder()
+                    .status(401)
+                    .timestamp(e.getTimestamp())
+                    .message(e.getMessage())
+                    .build();
+            response.setStatus(error.getStatus());
+            response.setCharacterEncoding("UTF-8");
+            response.setContentType("application/json;charset=UTF-8");
+            response.getWriter().write(mapper.writeValueAsString(error));
+            return;
+        }
+        jwt = token.token();
         String username = jwtService.extractUsername(jwt);
         if (username != null && SecurityContextHolder.getContext().getAuthentication() == null) {
-            @SuppressWarnings("unchecked") UserDetails details =
-                    new User(username, "", jwtService.extractClaim(jwt, claims ->
-                            claims.get("authorities", List.class).stream()
-                                    .map(o -> new SimpleGrantedAuthority(o.toString()))
-                                    .toList())
-                    );
-            System.out.println(details.getAuthorities().stream().map(GrantedAuthority::getAuthority).toList());
+            UserResponse user;
+            try {
+                user = resourceServerProxy.getUserByUsername(username);
+            } catch (NotFoundException e) {
+                ApiError error = ApiError.builder()
+                        .status(404)
+                        .timestamp(e.getTimestamp())
+                        .message(e.getMessage())
+                        .build();
+                response.setStatus(error.getStatus());
+                response.setCharacterEncoding("UTF-8");
+                response.setContentType("application/json;charset=UTF-8");
+                response.getWriter().write(mapper.writeValueAsString(error));
+                return;
+            }
+            UserDetails details =
+                    new User(username, "", resourceServerProxy.getUserAuthorities(user.id())
+                            .stream()
+                            .map(SimpleGrantedAuthority::new)
+                            .toList());
             if (jwtService.isTokenValid(jwt, details)) {
                 UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(
                         details, null, details.getAuthorities()
@@ -53,6 +95,7 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
                 SecurityContextHolder.getContext().setAuthentication(authToken);
             }
         }
+
         filterChain.doFilter(request, response);
     }
 }
