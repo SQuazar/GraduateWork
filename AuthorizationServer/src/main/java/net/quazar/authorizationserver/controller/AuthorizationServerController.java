62820f2fcf98225261b839c804b79d9f38ac6f92
SQuazar <flawesv@yandex.ru>
2023-05-18 23:17:40 +0300

    Removed extra methods

--- AuthorizationServer/src/main/java/net/quazar/authorizationserver/controller/AuthorizationServerController.java
+++ AuthorizationServer/src/main/java/net/quazar/authorizationserver/controller/AuthorizationServerController.java
@@ -16,7 +16,6 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 public class AuthorizationServerController {
     private final AuthenticationService authenticationService;
-    private final TokenService tokenService;
 
     @PostMapping("/login")
     public ResponseEntity<AuthenticationResponse> login(@RequestParam String username, @RequestParam String password) {
@@ -26,22 +25,5 @@ public class AuthorizationServerController {
     @PostMapping("/refreshToken")
     public ResponseEntity<TokenResponse> refreshToken(@RequestParam("refresh_token") String refreshToken) {
         return ResponseEntity.ok(authenticationService.refreshToken(refreshToken));
-    }
-
-    @PostMapping("/verifyToken")
-    public ResponseEntity<VerifyTokenResponse> verifyToken(@RequestHeader(HttpHeaders.AUTHORIZATION) String authorization) {
-        String bearer = "Bearer ";
-        if (authorization.length() <= bearer.length())
-            throw new InvalidTokenException("Неверный токен");
-        String token = authorization.substring(bearer.length());
-        return ResponseEntity.ok(authenticationService.verifyAccessToken(token));
-    }
-
-    @GetMapping("/token")
-    public ResponseEntity<TokenResponse> getToken(@RequestParam String token) {
-        Token res = tokenService.getByToken(token);
-        return ResponseEntity.ok(TokenResponse.builder()
-                .token(res.getToken())
-                .build());
     }
 }
