575cd040bab3d64457d1deed191e0c6136c4f966
SQuazar <flawesv@yandex.ru>
2023-05-18 23:18:12 +0300

    Import optimization

--- AuthorizationServer/src/main/java/net/quazar/authorizationserver/controller/AuthorizationServerController.java
+++ AuthorizationServer/src/main/java/net/quazar/authorizationserver/controller/AuthorizationServerController.java
@@ -3,14 +3,11 @@ package net.quazar.authorizationserver.controller;
 import lombok.AllArgsConstructor;
 import net.quazar.authorizationserver.controller.entity.AuthenticationResponse;
 import net.quazar.authorizationserver.controller.entity.TokenResponse;
-import net.quazar.authorizationserver.controller.entity.VerifyTokenResponse;
-import net.quazar.authorizationserver.entity.Token;
-import net.quazar.authorizationserver.exception.InvalidTokenException;
 import net.quazar.authorizationserver.service.AuthenticationService;
-import net.quazar.authorizationserver.service.TokenService;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 @AllArgsConstructor
 @RestController
