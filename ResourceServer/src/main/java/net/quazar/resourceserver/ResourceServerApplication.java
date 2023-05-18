1c8a91ad22d89e874a7bcfaab971da6c820f052a
SQuazar <flawesv@yandex.ru>
2023-05-18 23:09:35 +0300

    Added controller advice

--- ResourceServer/src/main/java/net/quazar/resourceserver/ResourceServerApplication.java
+++ ResourceServer/src/main/java/net/quazar/resourceserver/ResourceServerApplication.java
@@ -1,9 +1,12 @@
 package net.quazar.resourceserver;
 
+import net.quazar.resourceserver.exception.ResourceServerExceptionHandler;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration;
+import org.springframework.web.bind.annotation.ControllerAdvice;
 
+@ControllerAdvice(basePackageClasses = ResourceServerExceptionHandler.class)
 @SpringBootApplication(exclude = UserDetailsServiceAutoConfiguration.class)
 public class ResourceServerApplication {
 
