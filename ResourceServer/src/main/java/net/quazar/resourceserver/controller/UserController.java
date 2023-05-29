f01710cb49c9f3f240b3f33fa201372383e77550
SQuazar <flawesv@yandex.ru>
2023-05-29 05:22:12 +0300

    Added new methods for get permissions, update user and create user

--- ResourceServer/src/main/java/net/quazar/resourceserver/controller/UserController.java
+++ ResourceServer/src/main/java/net/quazar/resourceserver/controller/UserController.java
@@ -1,14 +1,14 @@
 package net.quazar.resourceserver.controller;
 
 import lombok.AllArgsConstructor;
+import lombok.Data;
 import net.quazar.resourceserver.entity.dto.RoleDto;
 import net.quazar.resourceserver.entity.dto.UserDto;
 import net.quazar.resourceserver.service.UserService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.lang.NonNull;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
 
@@ -38,8 +38,42 @@ public class UserController {
         return userService.getAuthorities(id);
     }
 
+    @GetMapping("/{id}/permissions")
+    public Set<String> getUserPermissions(@PathVariable int id) {
+        return userService.getPermissions(id);
+    }
+
     @GetMapping("/{id}/roles")
     public List<RoleDto> getUserRoles(@PathVariable int id) {
         return userService.getRoles(id);
+    }
+
+    @PutMapping
+    public UserDto createUser(@RequestBody CreateUserRequest request) {
+        return userService.createUser(request.username, request.password);
+    }
+
+    @PostMapping("/{id}")
+    public UserDto updateUser(@PathVariable int id, @RequestBody UpdateUserRequest request) {
+        return userService.update(id, request.username, request.password, request.roles, request.authorities);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteUser(@PathVariable int id) {
+        userService.delete(id);
+    }
+
+    @Data
+    static class CreateUserRequest {
+        private @NonNull String username;
+        private @NonNull String password;
+    }
+
+    @Data
+    static class UpdateUserRequest {
+        private @NonNull String username;
+        private @NonNull String password;
+        private @NonNull List<Integer> roles = new ArrayList<>();
+        private @NonNull List<String> authorities = new ArrayList<>();
     }
 }
