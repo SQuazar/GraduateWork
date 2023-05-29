4790372ff8d0b6e5cf82d2c252bfaef302239f31
SQuazar <flawesv@yandex.ru>
2023-05-29 05:28:37 +0300

    Added new methods and changed return type to dto class

--- ResourceServer/src/main/java/net/quazar/resourceserver/service/AnnouncementService.java
+++ ResourceServer/src/main/java/net/quazar/resourceserver/service/AnnouncementService.java
@@ -2,6 +2,9 @@ package net.quazar.resourceserver.service;
 
 import net.quazar.resourceserver.entity.Announcement;
 
+import java.util.List;
+
 public interface AnnouncementService {
-    Announcement createAnnouncement(String text);
+    Announcement createAnnouncement(String text, int sender, List<String> categories, List<String> roles);
+    List<Announcement> getAll();
 }
--- ResourceServer/src/main/java/net/quazar/resourceserver/service/impl/AnnouncementServiceImpl.java
+++ ResourceServer/src/main/java/net/quazar/resourceserver/service/impl/AnnouncementServiceImpl.java
@@ -2,19 +2,34 @@ package net.quazar.resourceserver.service.impl;
 
 import lombok.AllArgsConstructor;
 import net.quazar.resourceserver.entity.Announcement;
+import net.quazar.resourceserver.exception.UserNotFoundException;
 import net.quazar.resourceserver.repository.AnnouncementRepository;
+import net.quazar.resourceserver.repository.UserRepository;
 import net.quazar.resourceserver.service.AnnouncementService;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @AllArgsConstructor
 @Service
 public class AnnouncementServiceImpl implements AnnouncementService {
     private final AnnouncementRepository announcementRepository;
+    private final UserRepository userRepository;
 
     @Override
-    public Announcement createAnnouncement(String text) {
+    public Announcement createAnnouncement(String text, int sender, List<String> categories, List<String> roles) {
+        var user = userRepository.findById(sender)
+                .orElseThrow(() -> new UserNotFoundException("Пользователь %d не найден".formatted(sender)));
         return announcementRepository.save(Announcement.builder()
                 .text(text)
+                .sender(user)
+                .categories(String.join(",", categories))
+                .roles(String.join(",", roles))
                 .build());
+    }
+
+    @Override
+    public List<Announcement> getAll() {
+        return announcementRepository.findAll();
     }
 }
