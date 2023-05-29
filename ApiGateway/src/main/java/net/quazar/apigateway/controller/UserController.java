fdf5b4fb0c2b7ef4667b49c3610250846e3d8550
SQuazar <flawesv@yandex.ru>
2023-05-29 05:37:45 +0300

    Changed return type to ApiResponse and added new methods and added globally authority check for controller

--- ApiGateway/src/main/java/net/quazar/apigateway/controller/UserController.java
+++ ApiGateway/src/main/java/net/quazar/apigateway/controller/UserController.java
@@ -1,44 +1,113 @@
 package net.quazar.apigateway.controller;
 
 import lombok.AllArgsConstructor;
+import net.quazar.apigateway.entity.ApiResponse;
 import net.quazar.apigateway.proxy.ResourceServerProxy;
-import net.quazar.apigateway.proxy.resource.UserResponse;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
-import java.util.Set;
+import net.quazar.apigateway.proxy.resource.CreateUserRequest;
+import net.quazar.apigateway.proxy.resource.UpdateUserRequest;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
 
 @AllArgsConstructor
+@PreAuthorize("hasAuthority('users.get')")
 @RestController
 @RequestMapping("/api/users")
 public class UserController {
     private final ResourceServerProxy resourceServerProxy;
 
     @GetMapping
-    public List<UserResponse> getAllUsers() {
-        return resourceServerProxy.getAllUsers();
+    public ResponseEntity<ApiResponse> getAllUsers() {
+        return ResponseEntity.ok(
+                ApiResponse.builder()
+                        .code(200)
+                        .response(resourceServerProxy.getAllUsers())
+                        .build()
+        );
     }
 
     @GetMapping("/{id}")
-    public UserResponse getUserById(@PathVariable int id) {
-        return resourceServerProxy.getUserById(id);
+    public ResponseEntity<ApiResponse> getUserById(@PathVariable int id) {
+        return ResponseEntity.ok(
+                ApiResponse.builder()
+                        .code(200)
+                        .response(resourceServerProxy.getUserById(id))
+                        .build()
+        );
     }
 
     @GetMapping("/name/{username}")
-    public UserResponse getUserByUsername(@PathVariable String username) {
-        return resourceServerProxy.getUserByUsername(username);
+    public ResponseEntity<ApiResponse> getUserByUsername(@PathVariable String username) {
+        return ResponseEntity.ok(
+                ApiResponse.builder()
+                        .code(200)
+                        .response(resourceServerProxy.getUserByUsername(username))
+                        .build()
+        );
     }
 
     @GetMapping("/{id}/authorities")
-    public Set<String> getUserAuthorities(@PathVariable int id) {
-        return resourceServerProxy.getUserAuthorities(id);
+    public ResponseEntity<ApiResponse> getUserAuthorities(@PathVariable int id) {
+        return ResponseEntity.ok(
+                ApiResponse.builder()
+                        .code(200)
+                        .response(resourceServerProxy.getUserAuthorities(id))
+                        .build()
+        );
     }
 
+    @GetMapping("/{id}/permissions")
+    public ResponseEntity<ApiResponse> getUserPermissions(@PathVariable int id) {
+        return ResponseEntity.ok(
+                ApiResponse.builder()
+                        .code(200)
+                        .response(resourceServerProxy.getUserPermissions(id))
+                        .build()
+        );
+    }
+
+
     @GetMapping("/{id}/roles")
-    public List<UserResponse> getUserRoles(@PathVariable int id) {
-        return resourceServerProxy.getUserRoles(id);
+    public ResponseEntity<ApiResponse> getUserRoles(@PathVariable int id) {
+        return ResponseEntity.ok(
+                ApiResponse.builder()
+                        .code(200)
+                        .response(resourceServerProxy.getUserRoles(id))
+                        .build()
+        );
+    }
+
+    @PreAuthorize("hasAuthority('users.create')")
+    @PutMapping
+    public ResponseEntity<ApiResponse> createUser(@RequestBody CreateUserRequest request) {
+        return ResponseEntity.ok(
+                ApiResponse.builder()
+                        .code(200)
+                        .response(resourceServerProxy.createUser(request))
+                        .build()
+        );
+    }
+
+    @PreAuthorize("hasAuthority('users.update')")
+    @PostMapping("/{id}")
+    public ResponseEntity<ApiResponse> updateUser(@PathVariable int id, @RequestBody UpdateUserRequest request) {
+        return ResponseEntity.ok(
+                ApiResponse.builder()
+                        .code(200)
+                        .response(resourceServerProxy.updateUser(id, request))
+                        .build()
+        );
+    }
+
+    @PreAuthorize("hasAuthority('users.delete')")
+    @DeleteMapping("/{id}")
+    public ResponseEntity<ApiResponse> deleteUser(@PathVariable int id) {
+        resourceServerProxy.deleteUser(id);
+        return ResponseEntity.ok(
+                ApiResponse.builder()
+                        .code(200)
+                        .response("Пользователь удалён из системы")
+                        .build()
+        );
     }
 }
