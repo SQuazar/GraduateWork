b2e0b7d5bce5109e2e898febfcb02260809dfef3
SQuazar <flawesv@yandex.ru>
2023-05-29 05:25:48 +0300

    Added authorities field to RoleDto

--- ResourceServer/src/main/java/net/quazar/resourceserver/entity/dto/RoleDto.java
+++ ResourceServer/src/main/java/net/quazar/resourceserver/entity/dto/RoleDto.java
@@ -3,9 +3,14 @@ package net.quazar.resourceserver.entity.dto;
 import lombok.Builder;
 import lombok.Data;
 
+import java.util.HashSet;
+import java.util.Set;
+
 @Data
 @Builder
 public final class RoleDto {
     private Integer id;
     private String name;
+    @Builder.Default
+    private final Set<String> authorities = new HashSet<>();
 }
