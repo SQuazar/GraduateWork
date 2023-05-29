9a9f67908eaa02801cdc6a9810ce6cb686909267
SQuazar <flawesv@yandex.ru>
2023-05-29 05:34:59 +0300

    Added new proxy mappings

--- ApiGateway/src/main/java/net/quazar/apigateway/proxy/TelegramBotServerProxy.java
+++ ApiGateway/src/main/java/net/quazar/apigateway/proxy/TelegramBotServerProxy.java
@@ -3,6 +3,7 @@ package net.quazar.apigateway.proxy;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
@@ -14,6 +15,9 @@ public interface TelegramBotServerProxy {
     @PostMapping("/sendAnnouncements")
     void startSendingAnnouncements(@RequestBody SendAnnouncementsRequest request);
 
+    @GetMapping("/state")
+    BotStateResponse getState();
+
     @Data
     final class SendAnnouncementsRequest {
         private final String text;
@@ -22,4 +26,6 @@ public interface TelegramBotServerProxy {
         @JsonProperty("roles")
         private final Set<Integer> rolesIds = new HashSet<>();
     }
+
+    record BotStateResponse(int code, String message) {}
 }
