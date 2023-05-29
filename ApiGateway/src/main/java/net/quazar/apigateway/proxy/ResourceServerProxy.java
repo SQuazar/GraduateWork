a1fd7ca97c7f86e816bd7239aca012dfd9fa8e90
SQuazar <flawesv@yandex.ru>
2023-05-29 05:35:09 +0300

    Added new proxy mappings

--- ApiGateway/src/main/java/net/quazar/apigateway/proxy/ResourceServerProxy.java
+++ ApiGateway/src/main/java/net/quazar/apigateway/proxy/ResourceServerProxy.java
@@ -1,10 +1,6 @@
 package net.quazar.apigateway.proxy;
 
-import net.quazar.apigateway.proxy.resource.AnnouncementDto;
-import net.quazar.apigateway.proxy.resource.TokenResponse;
-import net.quazar.apigateway.proxy.resource.RoleRequest;
-import net.quazar.apigateway.proxy.resource.RoleResponse;
-import net.quazar.apigateway.proxy.resource.UserResponse;
+import net.quazar.apigateway.proxy.resource.*;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -16,10 +12,18 @@ import java.util.Set;
 @FeignClient(name = "resource-server")
 public interface ResourceServerProxy {
     @PostMapping("/announcements")
-    ResponseEntity<AnnouncementDto> saveAnnouncement(@RequestParam String text);
+    AnnouncementResponse saveAnnouncement(@RequestParam String text, @RequestParam int sender,
+                                          @RequestParam List<String> categories,
+                                          @RequestParam List<String> roles);
+
+    @GetMapping("/announcements")
+    List<AnnouncementResponse> getAllAnnouncements();
 
     @GetMapping("/roles")
     List<RoleResponse> getAllRoles();
+
+    @GetMapping("/roles/by")
+    List<RoleResponse> getAllRolesBy(@RequestParam List<Integer> ids);
 
     @GetMapping("/roles/{id}")
     RoleResponse getRoleById(@PathVariable int id);
@@ -30,10 +34,13 @@ public interface ResourceServerProxy {
     @GetMapping("/roles/{id}/users")
     List<UserResponse> getUsersByRole(@PathVariable int id);
 
-    @PostMapping("/roles")
+    @GetMapping("/roles/{id}/authorities")
+    Set<String> getRoleAuthorities(@PathVariable int id);
+
+    @PutMapping("/roles")
     RoleResponse createRole(@RequestParam String name);
 
-    @PatchMapping("/roles/{id}")
+    @PostMapping("/roles/{id}")
     RoleResponse updateRole(@PathVariable int id, @RequestBody RoleRequest roleDto);
 
     @DeleteMapping("/roles/{id}")
@@ -50,8 +57,21 @@ public interface ResourceServerProxy {
 
     @GetMapping("/users/{id}/authorities")
     Set<String> getUserAuthorities(@PathVariable int id);
+
+    @GetMapping("/users/{id}/permissions")
+    Set<String> getUserPermissions(@PathVariable int id);
+
     @GetMapping("/users/{id}/roles")
-    List<UserResponse> getUserRoles(@PathVariable int id);
+    List<RoleResponse> getUserRoles(@PathVariable int id);
+
+    @PutMapping("/users")
+    UserResponse createUser(@RequestBody CreateUserRequest request);
+
+    @PostMapping("/users/{id}")
+    UserResponse updateUser(@PathVariable int id, @RequestBody UpdateUserRequest request);
+
+    @DeleteMapping("/{id}")
+    void deleteUser(@PathVariable int id);
 
     @GetMapping("/tokens/{token}")
     TokenResponse getToken(@PathVariable String token);
@@ -84,4 +104,31 @@ public interface ResourceServerProxy {
 
     @DeleteMapping("/tokens/revoked")
     void deleteRevokedTokens();
+
+    @GetMapping("/telegram/subscribe/categories")
+    List<CategoryResponse> getCategories();
+
+    @GetMapping("/telegram/subscribe/categories")
+    List<CategoryResponse> getCategories(@RequestParam List<Integer> ids);
+
+    @GetMapping("/telegram/users")
+    List<TelegramUserResponse> getAllTelegramUsers();
+
+    @GetMapping("/telegram/users/{id}")
+    TelegramUserResponse getTelegramUser(@PathVariable Long id);
+
+    @GetMapping("/telegram/users/{id}/roles")
+    List<RoleResponse> getTelegramUserRoles(@PathVariable Long id);
+
+    @GetMapping("/telegram/users/{id}/categories")
+    List<CategoryResponse> getTelegramUserCategories(@PathVariable Long id);
+
+    @PostMapping("/telegram/users/{id}")
+    TelegramUserResponse updateTelegramUser(@PathVariable Long id, @RequestBody TelegramUserUpdateRequest request);
+
+    @DeleteMapping("/categories/{id}")
+    void deleteCategory(@PathVariable int id);
+
+    @PutMapping("/categories")
+    CategoryResponse createCategory(@RequestParam String name);
 }
