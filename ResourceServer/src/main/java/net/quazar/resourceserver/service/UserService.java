cb2cae70cfe40a6daf0ae208c5dc8c9fe52fb8fd
SQuazar <flawesv@yandex.ru>
2023-05-29 05:21:46 +0300

    Added new methods for get permissions, update user and create user

--- ResourceServer/src/main/java/net/quazar/resourceserver/service/UserService.java
+++ ResourceServer/src/main/java/net/quazar/resourceserver/service/UserService.java
@@ -16,8 +16,11 @@ public interface UserService {
     List<RoleDto> getRoles(int userId);
 
     Set<String> getAuthorities(int userId);
+    Set<String> getPermissions(int userId);
 
     UserDto save(@NonNull UserDto entity);
+    UserDto update(int userId, String username, String password, List<Integer> roles, List<String> permissions);
+    UserDto createUser(String username, String password);
 
     void delete(int userId);
 }
