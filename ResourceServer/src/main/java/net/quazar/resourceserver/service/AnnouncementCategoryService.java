e54159ff2a3da0dba516b3a2489aa1a7860d7eba
SQuazar <flawesv@yandex.ru>
2023-05-29 05:28:24 +0300

    Added new methods and changed return type to dto class

--- ResourceServer/src/main/java/net/quazar/resourceserver/service/AnnouncementCategoryService.java
+++ ResourceServer/src/main/java/net/quazar/resourceserver/service/AnnouncementCategoryService.java
@@ -1,9 +1,15 @@
 package net.quazar.resourceserver.service;
 
-import net.quazar.resourceserver.entity.AnnouncementCategory;
+import net.quazar.resourceserver.entity.dto.CategoryDto;
 
 import java.util.List;
 
 public interface AnnouncementCategoryService {
-    List<AnnouncementCategory> getAll();
+    List<CategoryDto> getAll();
+
+    List<CategoryDto> getAllByIds(List<Integer> ids);
+
+    CategoryDto create(String name);
+
+    void deleteById(int id);
 }
--- ResourceServer/src/main/java/net/quazar/resourceserver/service/impl/AnnouncementCategoryServiceImpl.java
+++ ResourceServer/src/main/java/net/quazar/resourceserver/service/impl/AnnouncementCategoryServiceImpl.java
@@ -2,6 +2,10 @@ package net.quazar.resourceserver.service.impl;
 
 import lombok.AllArgsConstructor;
 import net.quazar.resourceserver.entity.AnnouncementCategory;
+import net.quazar.resourceserver.entity.dto.CategoryDto;
+import net.quazar.resourceserver.exception.AnnouncementCategoryAlreadyExists;
+import net.quazar.resourceserver.exception.AnnouncementCategoryNotFound;
+import net.quazar.resourceserver.mapper.CategoryDtoMapper;
 import net.quazar.resourceserver.repository.AnnouncementCategoryRepository;
 import net.quazar.resourceserver.service.AnnouncementCategoryService;
 import org.springframework.stereotype.Service;
@@ -12,9 +16,39 @@ import java.util.List;
 @Service
 public class AnnouncementCategoryServiceImpl implements AnnouncementCategoryService {
     private final AnnouncementCategoryRepository categoryRepository;
+    private final CategoryDtoMapper mapper = CategoryDtoMapper.INSTANCE;
 
     @Override
-    public List<AnnouncementCategory> getAll() {
-        return categoryRepository.findAll();
+    public List<CategoryDto> getAll() {
+        return categoryRepository.findAll()
+                .stream()
+                .map(mapper::mapToDto)
+                .toList();
+    }
+
+    @Override
+    public List<CategoryDto> getAllByIds(List<Integer> ids) {
+        return categoryRepository.findAllById(ids)
+                .stream()
+                .map(mapper::mapToDto)
+                .toList();
+    }
+
+    @Override
+    public CategoryDto create(String name) {
+        categoryRepository.findByName(name).ifPresent(res -> {
+            throw new AnnouncementCategoryAlreadyExists("Категория '%s' уже существует".formatted(name));
+        });
+        return mapper.mapToDto(categoryRepository.save(AnnouncementCategory.builder()
+                .name(name)
+                .build()));
+    }
+
+    @Override
+    public void deleteById(int id) {
+        var findId = categoryRepository.findById(id)
+                .orElseThrow(() -> new AnnouncementCategoryNotFound("Категория с ID %d не найдена".formatted(id)))
+                .getId();
+        categoryRepository.deleteById(findId);
     }
 }
