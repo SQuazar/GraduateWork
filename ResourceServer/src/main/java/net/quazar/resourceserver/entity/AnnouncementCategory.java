aa26b5eb0af3d74f61ac040ce94efb1fc2cb01ec
SQuazar <flawesv@yandex.ru>
2023-05-29 05:27:44 +0300

    Added builder annotation and on delete action for ManyToMany relationship

--- ResourceServer/src/main/java/net/quazar/resourceserver/entity/AnnouncementCategory.java
+++ ResourceServer/src/main/java/net/quazar/resourceserver/entity/AnnouncementCategory.java
@@ -2,6 +2,8 @@ package net.quazar.resourceserver.entity;
 
 import jakarta.persistence.*;
 import lombok.*;
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -13,10 +15,12 @@ import java.util.List;
 @Data
 @ToString(exclude = "users")
 @EqualsAndHashCode(exclude = "users")
+@Builder
 public class AnnouncementCategory {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private @Id Integer id;
     private @Column(nullable = false, unique = true) String name;
     @ManyToMany(mappedBy = "categories")
+    @OnDelete(action = OnDeleteAction.CASCADE)
     private List<TelegramUser> users = new ArrayList<>();
 }
