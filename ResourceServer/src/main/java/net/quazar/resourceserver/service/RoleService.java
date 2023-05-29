328910bd4727be6fb6c4d48a45d2b0c21d208fd4
SQuazar <flawesv@yandex.ru>
2023-05-29 05:25:13 +0300

    Added new methods for RoleService

--- ResourceServer/src/main/java/net/quazar/resourceserver/service/RoleService.java
+++ ResourceServer/src/main/java/net/quazar/resourceserver/service/RoleService.java
@@ -5,12 +5,17 @@ import net.quazar.resourceserver.entity.dto.UserDto;
 import org.springframework.lang.NonNull;
 
 import java.util.List;
+import java.util.Set;
 
 public interface RoleService {
     RoleDto getById(int id);
+    List<RoleDto> getAllByIds(List<Integer> ids);
     RoleDto getByName(@NonNull String name);
     List<RoleDto> getAll();
     List<UserDto> getUsers(int roleId);
+    Set<String> getAuthorities(int roleId);
     RoleDto save(@NonNull RoleDto entity);
+    RoleDto create(@NonNull RoleDto entity);
+
     void delete(int roleId);
 }
