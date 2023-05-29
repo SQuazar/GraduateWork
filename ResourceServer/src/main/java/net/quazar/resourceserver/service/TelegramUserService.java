1f97a21048ec37e46d5ab0f62f648360ff36ad0e
SQuazar <flawesv@yandex.ru>
2023-05-29 05:23:49 +0300

    Added new methods and changed old methods and response type from TelegramUser to TelegramUserDto

--- ResourceServer/src/main/java/net/quazar/resourceserver/service/TelegramUserService.java
+++ ResourceServer/src/main/java/net/quazar/resourceserver/service/TelegramUserService.java
@@ -1,12 +1,19 @@
 package net.quazar.resourceserver.service;
 
-import net.quazar.resourceserver.entity.TelegramUser;
+import net.quazar.resourceserver.entity.dto.CategoryDto;
+import net.quazar.resourceserver.entity.dto.RoleDto;
+import net.quazar.resourceserver.entity.dto.TelegramUserDto;
 
 import java.util.List;
 
 public interface TelegramUserService {
-    List<TelegramUser> getAll();
-    List<TelegramUser> getAllByCategoryId(int categoryId);
-    List<TelegramUser> getAllByRoleId(int roleId);
-    List<TelegramUser> getAllByRoleIdAndCategoryId(int roleId, int categoryId);
+    List<TelegramUserDto> getAll();
+    TelegramUserDto getById(Long id);
+    TelegramUserDto update(Long id, String signature, List<Integer> roles, List<Integer> categories);
+    List<TelegramUserDto> getAllByCategoryId(int categoryId);
+    List<TelegramUserDto> getAllByRoleId(int roleId);
+
+    List<TelegramUserDto> getAllByRoleIdAndCategoryId(int roleId, int categoryId);
+    List<RoleDto> getRolesByUserId(Long id);
+    List<CategoryDto> getCategoriesByUserId(Long id);
 }
--- ResourceServer/src/main/java/net/quazar/resourceserver/service/impl/TelegramUserServiceImpl.java
+++ ResourceServer/src/main/java/net/quazar/resourceserver/service/impl/TelegramUserServiceImpl.java
@@ -1,35 +1,104 @@
 package net.quazar.resourceserver.service.impl;
 
 import lombok.AllArgsConstructor;
-import net.quazar.resourceserver.entity.TelegramUser;
+import net.quazar.resourceserver.entity.dto.CategoryDto;
+import net.quazar.resourceserver.entity.dto.RoleDto;
+import net.quazar.resourceserver.entity.dto.TelegramUserDto;
+import net.quazar.resourceserver.exception.TelegramUserNotFoundException;
+import net.quazar.resourceserver.mapper.CategoryDtoMapper;
+import net.quazar.resourceserver.mapper.RoleDtoMapper;
+import net.quazar.resourceserver.mapper.TelegramUserDtoMapper;
 import net.quazar.resourceserver.repository.TelegramUserRepository;
+import net.quazar.resourceserver.service.AnnouncementCategoryService;
+import net.quazar.resourceserver.service.RoleService;
 import net.quazar.resourceserver.service.TelegramUserService;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @AllArgsConstructor
 @Service
 public class TelegramUserServiceImpl implements TelegramUserService {
     private final TelegramUserRepository telegramUserRepository;
+    private final RoleService roleService;
+    private final AnnouncementCategoryService categoryService;
+    private final TelegramUserDtoMapper userDtoMapper = TelegramUserDtoMapper.INSTANCE;
+    private final RoleDtoMapper roleDtoMapper = RoleDtoMapper.INSTANCE;
+    private final CategoryDtoMapper categoryDtoMapper = CategoryDtoMapper.INSTANCE;
 
     @Override
-    public List<TelegramUser> getAll() {
-        return telegramUserRepository.findAll();
+    public List<TelegramUserDto> getAll() {
+        return telegramUserRepository.findAll()
+                .stream()
+                .map(userDtoMapper::mapToDto)
+                .toList();
     }
 
     @Override
-    public List<TelegramUser> getAllByCategoryId(int categoryId) {
-        return telegramUserRepository.findAllByCategoryId(categoryId);
+    public TelegramUserDto getById(Long id) {
+        return telegramUserRepository.findById(id)
+                .map(userDtoMapper::mapToDto)
+                .orElseThrow(() -> new TelegramUserNotFoundException("Пользователь %d не найден".formatted(id)));
     }
 
     @Override
-    public List<TelegramUser> getAllByRoleId(int roleId) {
-        return telegramUserRepository.findAllByRoleId(roleId);
+    public TelegramUserDto update(Long id, String signature, List<Integer> roles, List<Integer> categories) {
+        var user = telegramUserRepository.findById(id)
+                .orElseThrow(() -> new TelegramUserNotFoundException("Пользователь %d не найден".formatted(id)));
+        user.setRoles(roleService.getAllByIds(roles)
+                .stream()
+                .map(roleDtoMapper::dtoToRole)
+                .collect(Collectors.toSet()));
+        user.setCategories(categoryService.getAllByIds(categories)
+                .stream()
+                .map(categoryDtoMapper::mapFromDto)
+                .collect(Collectors.toSet()));
+        user.setSignature(signature);
+        return userDtoMapper.mapToDto(telegramUserRepository.save(user));
     }
 
     @Override
-    public List<TelegramUser> getAllByRoleIdAndCategoryId(int roleId, int categoryId) {
-        return telegramUserRepository.findAllByRoleIdAndCategoryId(roleId, categoryId);
+    public List<TelegramUserDto> getAllByCategoryId(int categoryId) {
+        return telegramUserRepository.findAllByCategoryId(categoryId)
+                .stream()
+                .map(userDtoMapper::mapToDto)
+                .toList();
+    }
+
+    @Override
+    public List<TelegramUserDto> getAllByRoleId(int roleId) {
+        return telegramUserRepository.findAllByRoleId(roleId)
+                .stream()
+                .map(userDtoMapper::mapToDto)
+                .toList();
+    }
+
+    @Override
+    public List<TelegramUserDto> getAllByRoleIdAndCategoryId(int roleId, int categoryId) {
+        return telegramUserRepository.findAllByRoleIdAndCategoryId(roleId, categoryId)
+                .stream()
+                .map(userDtoMapper::mapToDto)
+                .toList();
+    }
+
+    @Override
+    public List<RoleDto> getRolesByUserId(Long id) {
+        return telegramUserRepository.findById(id)
+                .orElseThrow(() -> new TelegramUserNotFoundException("Пользователь %d не найден".formatted(id)))
+                .getRoles()
+                .stream()
+                .map(roleDtoMapper::roleToDto)
+                .toList();
+    }
+
+    @Override
+    public List<CategoryDto> getCategoriesByUserId(Long id) {
+        return telegramUserRepository.findById(id)
+                .orElseThrow(() -> new TelegramUserNotFoundException("Пользователь %d не найден".formatted(id)))
+                .getCategories()
+                .stream()
+                .map(categoryDtoMapper::mapToDto)
+                .toList();
     }
 }
