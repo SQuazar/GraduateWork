95573720d9ea69f74db401edf255ef4856b4c732
SQuazar <flawesv@yandex.ru>
2023-05-29 05:25:56 +0300

    Added authorities field to RoleDto

--- ResourceServer/src/main/java/net/quazar/resourceserver/mapper/RoleDtoMapper.java
+++ ResourceServer/src/main/java/net/quazar/resourceserver/mapper/RoleDtoMapper.java
@@ -3,13 +3,16 @@ package net.quazar.resourceserver.mapper;
 import net.quazar.resourceserver.entity.Role;
 import net.quazar.resourceserver.entity.dto.RoleDto;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.factory.Mappers;
 
 @Mapper
 public interface RoleDtoMapper {
     RoleDtoMapper INSTANCE = Mappers.getMapper(RoleDtoMapper.class);
 
+    @Mapping(source = "permissions", target = "authorities")
     RoleDto roleToDto(Role role);
 
+    @Mapping(source = "authorities", target = "permissions")
     Role dtoToRole(RoleDto dto);
 }
