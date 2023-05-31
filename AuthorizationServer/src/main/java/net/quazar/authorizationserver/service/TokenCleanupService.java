4a37bf12b52c90be0ac0594cbbedba622867df7f
SQuazar <flawesv@yandex.ru>
2023-05-31 22:55:01 +0300

    Changed cron to delay

--- AuthorizationServer/src/main/java/net/quazar/authorizationserver/service/TokenCleanupService.java
+++ AuthorizationServer/src/main/java/net/quazar/authorizationserver/service/TokenCleanupService.java
@@ -4,12 +4,15 @@ import lombok.AllArgsConstructor;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
+import java.util.concurrent.TimeUnit;
+
 @AllArgsConstructor
 @Service
 public class TokenCleanupService {
     private final TokenService tokenService;
 
-    @Scheduled(cron = "0 0 0 * * *", zone = "Europe/Moscow")
+//    @Scheduled(cron = "0 0 0 * * *", zone = "Europe/Moscow")
+    @Scheduled(fixedDelay = 60, initialDelay = 1, timeUnit = TimeUnit.MINUTES)
     public void cleanTokens() {
         tokenService.deleteRevokedTokens();
         tokenService.deleteExpiredTokens();
