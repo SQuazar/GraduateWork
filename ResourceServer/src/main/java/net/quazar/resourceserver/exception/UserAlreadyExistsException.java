557d2ce82bfbb30445033f759ccddb9b0508efcb
SQuazar <flawesv@yandex.ru>
2023-05-31 22:53:25 +0300

    UserAlreadyExistsException moved to exceptions folder

--- ResourceServer/src/main/java/net/quazar/resourceserver/exception/UserAlreadyExistsException.java
+++ ResourceServer/src/main/java/net/quazar/resourceserver/exception/UserAlreadyExistsException.java
@@ -1,4 +1,4 @@
-package net.quazar.resourceserver.entity;
+package net.quazar.resourceserver.exception;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
