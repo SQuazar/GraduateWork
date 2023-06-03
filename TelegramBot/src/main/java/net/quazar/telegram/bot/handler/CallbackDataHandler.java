90e39a0080425063ae85bc5bf026379163a5231b
SQuazar <flawesv@yandex.ru>
2023-06-03 23:26:11 +0300

    Added CallbackQuery to handle params

--- TelegramBot/src/main/java/net/quazar/telegram/bot/handler/CallbackDataHandler.java
+++ TelegramBot/src/main/java/net/quazar/telegram/bot/handler/CallbackDataHandler.java
@@ -1,13 +1,14 @@
 package net.quazar.telegram.bot.handler;
 
 import org.springframework.stereotype.Component;
+import org.telegram.telegrambots.meta.api.objects.CallbackQuery;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.bots.AbsSender;
 
 import java.lang.annotation.*;
 
 public interface CallbackDataHandler {
-    void handle(Update update, AbsSender sender);
+    void handle(Update update, CallbackQuery query, AbsSender sender);
 
     @Component
     @Inherited
