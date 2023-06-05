15aefea9d080059f6d3eb641860ea638d99aebb4
SQuazar <flawesv@yandex.ru>
2023-06-05 03:45:31 +0300

    Added inline keyboard update integration and exception handling

--- TelegramBot/src/main/java/net/quazar/telegram/bot/AnnouncementsBot.java
+++ TelegramBot/src/main/java/net/quazar/telegram/bot/AnnouncementsBot.java
@@ -1,6 +1,8 @@
 package net.quazar.telegram.bot;
 
 import feign.FeignException;
+import net.quazar.telegram.bot.exception.SomethingIsWrongException;
+import net.quazar.telegram.bot.exception.SubscriptionNotFoundException;
 import net.quazar.telegram.bot.handler.CallbackDataHandler;
 import net.quazar.telegram.bot.handler.registry.CallbackDataHandlerRegistry;
 import net.quazar.telegram.bot.handler.registry.impl.AnnouncementBotStateHandlerRegistry;
@@ -46,65 +48,8 @@ public class AnnouncementsBot extends TelegramLongPollingBot {
     @Override
     public void onUpdateReceived(Update update) {
         if (update.hasCallbackQuery()) {
-            CallbackQuery query = update.getCallbackQuery();
-            String callback = query.getData();
-            var handlers = callbackDataHandlerRegistry.getHandlers(callback);
-            if (handlers != null) {
-                for (CallbackDataHandler handler : handlers)
-                    handler.handle(update, query, this);
-            } else {
-                SendMessage sendMessage = SendMessage.builder()
-                        .chatId(update.getMessage().getChatId())
-                        .text("Ошибка. Недействительная кнопка")
-                        .replyMarkup(Keyboards.getKeyboardByState(StateHandler.State.MENU))
-                        .build();
-                try {
-                    sendApiMethod(sendMessage);
-                } catch (TelegramApiException e) {
-                    LOGGER.warn("Cannot execute api method", e);
-                }
-                return;
-            }
-        }
-        if (!update.hasMessage()) return;
-        if (!update.getMessage().hasText()) return;
-        int state;
-        try {
-            state = resourceServerProxy.getSubscription(update.getMessage().getFrom().getId()).getBody().state();
-        } catch (FeignException e) {
-            state = 0;
-        }
-        if (update.getMessage().getText().equals("/start") && state == 0) {
-            SendMessage sendMessage = SendMessage.builder()
-                    .chatId(update.getMessage().getChatId())
-                    .text("Для подписки на новостную рассылку нажмите кнопку \"Подписаться\"")
-                    .replyMarkup(Keyboards.getKeyboardByState(StateHandler.State.START))
-                    .build();
             try {
-                sendApiMethod(sendMessage);
-            } catch (TelegramApiException e) {
-                LOGGER.warn("Cannot execute api method", e);
-            }
-            return;
-        }
-        var handlers = stateHandlerRegistry.getStateHandlers(state);
-        if (handlers == null) {
-            SendMessage message = SendMessage.builder()
-                    .chatId(update.getMessage().getFrom().getId())
-                    .text("Неверный ввод. Возвращаем в главное меню.")
-                    .replyMarkup(Keyboards.startKeyboard())
-                    .build();
-            try {
-                sendApiMethod(message);
-            } catch (TelegramApiException e) {
-                LOGGER.warn("Cannot execute api method", e);
-            }
-            return;
-        }
-        handlers.forEach(handler -> {
-            int nextState = 0;
-            try {
-                nextState = handler.handle(update, this);
+                handleCallbackQueryUpdate(update);
             } catch (TelegramApiRequestException e) {
                 LOGGER.warn("Cannot execute api method", e);
                 if (e.getApiResponse().contains("Can't access the chat") || e.getApiResponse().contains("Bot was blocked by the user")) {
@@ -117,34 +62,121 @@ public class AnnouncementsBot extends TelegramLongPollingBot {
             } catch (TelegramApiException e) {
                 LOGGER.warn("Cannot execute api method", e);
             }
-            if (nextState == 0) return;
-            try {
-                resourceServerProxy.changeState(update.getMessage().getFrom().getId(), nextState);
-            } catch (FeignException.NotFound e) {
-                SendMessage sendMessage = SendMessage.builder()
-                        .chatId(update.getMessage().getFrom().getId())
-                        .text("Подписка не найдена")
-                        .replyMarkup(Keyboards.getKeyboardByState(StateHandler.State.START))
-                        .build();
+            return;
+        }
+        if (!update.hasMessage()) return;
+        if (!update.getMessage().hasText()) return;
+        try {
+            handleMessageUpdate(update);
+        } catch (TelegramApiRequestException e) {
+            LOGGER.warn("Cannot execute api method", e);
+            if (e.getApiResponse().contains("Can't access the chat") || e.getApiResponse().contains("Bot was blocked by the user")) {
                 try {
-                    sendApiMethod(sendMessage);
-                } catch (TelegramApiException ex) {
-                    LOGGER.warn("Cannot execute api method", e);
-                }
-            } catch (FeignException e) {
-                SendMessage sendMessage = SendMessage.builder()
-                        .chatId(update.getMessage().getFrom().getId())
-                        .text("Что-то пошло не так.")
-                        .replyMarkup(Keyboards.getKeyboardByState(StateHandler.State.START))
-                        .build();
-                LOGGER.warn("{} Не удалось получить подписку пользователя: {}", e.status(), e.contentUTF8());
-                try {
-                    sendApiMethod(sendMessage);
-                } catch (TelegramApiException ex) {
-                    LOGGER.warn("Cannot execute api method", e);
+                    resourceServerProxy.unsubscribe(update.getMessage().getFrom().getId());
+                } catch (FeignException exception) {
+                    LOGGER.warn("{} Something went is wrong: {}", exception.status(), exception.contentUTF8());
                 }
             }
-        });
+        } catch (TelegramApiException e) {
+            LOGGER.warn("Cannot execute api method", e);
+        }
+    }
+
+    private void handleCallbackQueryUpdate(Update update) throws TelegramApiException {
+        CallbackQuery query = update.getCallbackQuery();
+        String callback = query.getData();
+        var handlers = callbackDataHandlerRegistry.getHandlers(callback);
+        if (handlers != null) {
+            for (CallbackDataHandler handler : handlers) {
+                try {
+                    handler.handle(update, query, this);
+                } catch (SubscriptionNotFoundException e) {
+                    SendMessage sendMessage = SendMessage.builder()
+                            .text(e.getMessage())
+                            .replyMarkup(Keyboards.getKeyboardByState(StateHandler.State.START))
+                            .build();
+                    sendApiMethod(sendMessage);
+                } catch (SomethingIsWrongException e) {
+                    SendMessage sendMessage = SendMessage.builder()
+                            .text(e.getMessage())
+                            .replyMarkup(Keyboards.getKeyboardByState(StateHandler.State.MENU))
+                            .build();
+                    sendApiMethod(sendMessage);
+                }
+            }
+        } else {
+            SendMessage sendMessage = SendMessage.builder()
+                    .chatId(query.getMessage().getChatId())
+                    .text("Ошибка. Недействительная кнопка")
+                    .replyMarkup(Keyboards.getKeyboardByState(StateHandler.State.MENU))
+                    .build();
+            sendApiMethod(sendMessage);
+        }
+    }
+
+    private void handleMessageUpdate(Update update) throws TelegramApiException {
+        int state;
+        try {
+            state = resourceServerProxy.getSubscription(update.getMessage().getFrom().getId()).getBody().state();
+        } catch (FeignException e) {
+            LOGGER.warn("Cannot get user subscription {}", e.getMessage());
+            state = 0;
+        }
+        if (update.getMessage().getText().equals("/start") && state == 0) {
+            SendMessage sendMessage = SendMessage.builder()
+                    .chatId(update.getMessage().getChatId())
+                    .text("Для подписки на новостную рассылку нажмите кнопку \"Подписаться\"")
+                    .replyMarkup(Keyboards.getKeyboardByState(StateHandler.State.START))
+                    .build();
+            sendApiMethod(sendMessage);
+
+            return;
+        }
+        var handlers = stateHandlerRegistry.getStateHandlers(state);
+        if (handlers == null) {
+            SendMessage message = SendMessage.builder()
+                    .chatId(update.getMessage().getFrom().getId())
+                    .text("Неверный ввод. Возвращаем в главное меню.")
+                    .replyMarkup(Keyboards.getKeyboardByState(state))
+                    .build();
+            sendApiMethod(message);
+            return;
+        }
+        for (StateHandler handler : handlers) {
+            int nextState;
+            nextState = handler.handle(update, this);
+            if (nextState == 0) return;
+            changeSubscriberState(update.getMessage().getFrom().getId(), nextState);
+        }
+    }
+
+    public void changeSubscriberState(long userId, int state) {
+        try {
+            resourceServerProxy.changeState(userId, state);
+        } catch (FeignException.NotFound e) {
+            SendMessage sendMessage = SendMessage.builder()
+                    .chatId(userId)
+                    .text("Подписка не найдена")
+                    .replyMarkup(Keyboards.getKeyboardByState(StateHandler.State.START))
+                    .build();
+            try {
+                sendApiMethod(sendMessage);
+            } catch (TelegramApiException ex) {
+                LOGGER.warn("Cannot execute api method", e);
+            }
+        } catch (FeignException e) {
+            SendMessage sendMessage = SendMessage.builder()
+                    .chatId(userId)
+                    .text("Что-то пошло не так.")
+                    .replyMarkup(Keyboards.getKeyboardByState(StateHandler.State.START))
+                    .build();
+            LOGGER.warn("{} Не удалось получить подписку пользователя: {}", e.status(), e.contentUTF8());
+            try {
+                sendApiMethod(sendMessage);
+            } catch (TelegramApiException ex) {
+                LOGGER.warn("Cannot execute api method", e);
+            }
+        }
     }
 
     public StateHandlerRegistry getStateHandlerRegistry() {
