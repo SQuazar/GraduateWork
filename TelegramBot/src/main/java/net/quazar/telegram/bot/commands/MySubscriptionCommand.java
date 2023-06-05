9ce3b17ae8cb4674d6d16e007f488025917811b5
SQuazar <flawesv@yandex.ru>
2023-06-05 04:03:37 +0300

    Most logic delegated to SubscriptionService and added inline keyboard integration

--- TelegramBot/src/main/java/net/quazar/telegram/bot/commands/MySubscriptionCommand.java
+++ TelegramBot/src/main/java/net/quazar/telegram/bot/commands/MySubscriptionCommand.java
@@ -1,53 +1,28 @@
 package net.quazar.telegram.bot.commands;
 
-import feign.FeignException;
 import lombok.AllArgsConstructor;
-import net.quazar.telegram.bot.Keyboards;
+import net.quazar.telegram.bot.handler.CallbackDataHandler;
 import net.quazar.telegram.bot.handler.StateHandler;
-import net.quazar.telegram.proxy.ResourceServerProxy;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.http.ResponseEntity;
+import net.quazar.telegram.bot.service.SubscriptionService;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
-import org.telegram.telegrambots.meta.api.objects.Message;
 import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.InlineKeyboardMarkup;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.InlineKeyboardButton;
 import org.telegram.telegrambots.meta.bots.AbsSender;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
 import java.time.format.DateTimeFormatter;
+import java.util.List;
 
 @AllArgsConstructor
 public class MySubscriptionCommand implements BotCommand {
-    private static final Logger LOGGER = LoggerFactory.getLogger(MySubscriptionCommand.class);
 
-    private final ResourceServerProxy resourceServerProxy;
+    private final SubscriptionService subscriptionService;
     private final DateTimeFormatter dateTimeFormatter;
 
     @Override
-    public int execute(Update update, AbsSender absSender) throws TelegramApiException {
-        Message message = update.getMessage();
-        ResponseEntity<ResourceServerProxy.SubscriptionResponse> response;
-        try {
-            response = resourceServerProxy.getSubscription(message.getFrom().getId());
-        } catch (FeignException.NotFound e) {
-            SendMessage sendMessage = SendMessage.builder()
-                    .chatId(message.getFrom().getId())
-                    .text("Подписка не найдена")
-                    .replyMarkup(Keyboards.getKeyboardByState(StateHandler.State.START))
-                    .build();
-            absSender.execute(sendMessage);
-            return StateHandler.State.START;
-        } catch (FeignException e) {
-            SendMessage sendMessage = SendMessage.builder()
-                    .chatId(message.getFrom().getId())
-                    .text("Что-то пошло не так.")
-                    .replyMarkup(Keyboards.getKeyboardByState(StateHandler.State.START))
-                    .build();
-            absSender.execute(sendMessage);
-            LOGGER.warn("{} Не удалось получить подписку пользователя: {}",e.status(), e.contentUTF8());
-            return StateHandler.State.MENU;
-        }
-        var subscription = response.getBody();
+    public int execute(Update update, long chatId, AbsSender absSender) throws TelegramApiException {
+        var subscription = subscriptionService.getSubscription(chatId);
         StringBuilder subscriptionInfo = new StringBuilder("Ваша подписка: \n");
         subscriptionInfo.append("Ваш ID: ").append(subscription.userId()).append('\n')
                 .append("Подписан с: ")
@@ -55,11 +30,22 @@ public class MySubscriptionCommand implements BotCommand {
         if (subscription.roles() != null && !subscription.roles().isEmpty())
             subscriptionInfo.append("Ваши роли: ").append(String.join(", ", subscription.roles()));
         SendMessage sendMessage = SendMessage.builder()
-                .chatId(message.getFrom().getId())
+                .chatId(chatId)
                 .text(subscriptionInfo.toString())
-                .replyMarkup(Keyboards.getKeyboardByState(StateHandler.State.MY_SUBSCRIPTION))
+                .replyMarkup(InlineKeyboardMarkup.builder()
+                        .keyboard(List.of(
+                                List.of(InlineKeyboardButton.builder()
+                                        .text("Мои категории")
+                                        .callbackData(CallbackDataHandler.Callbacks.MY_CATEGORIES)
+                                        .build()),
+                                List.of(InlineKeyboardButton.builder()
+                                        .text("Отменить подписку")
+                                        .callbackData(CallbackDataHandler.Callbacks.UNSUBSCRIBE)
+                                        .build())
+                        ))
+                        .build())
                 .build();
         absSender.execute(sendMessage);
-        return StateHandler.State.MY_SUBSCRIPTION;
+        return StateHandler.State.MENU;
     }
 }
