44a925ebd98b9eb72f1614ae6c82092b6254923f
SQuazar <flawesv@yandex.ru>
2023-06-05 03:44:43 +0300

    Removed most text commands

--- TelegramBot/src/main/java/net/quazar/telegram/bot/commands/TextCommands.java
+++ TelegramBot/src/main/java/net/quazar/telegram/bot/commands/TextCommands.java
@@ -6,27 +6,6 @@ public final class TextCommands {
     }
 
     public static final class MenuCommands {
-        public static final String MY_SUBSCRIPTION = "Моя подписка";
+        public static final String MY_SUBSCRIPTION = "\uD83D\uDC64Моя подписка\uD83D\uDC64";
     }
-
-    public static final class MySubscriptionCommands {
-        public static final String CATEGORIES = "Категории";
-        public static final String UNSUBSCRIBE = "Отписаться";
-    }
-
-    public static final class MyCategoriesCommands {
-        public static final String SUBSCRIBE_CATEGORY = "Подписаться на категорию";
-        public static final String UNSUBSCRIBE_CATEGORY = "Отписаться от категории";
-        public static final String GO_BACK = "Назад";
-    }
-
-    public static final class AddCategoryCommands {
-        public static final String DONE = "Готово";
-    }
-
-    public static final class RemoveCategoryCommands {
-        public static final String DONE = "Готово";
-    }
-
-    public static final String BACK_TO_MENU = "Назад в меню";
 }
