a935541acb7d2d70a29734733dfdedd1d3eea4ff
SQuazar <flawesv@yandex.ru>
2023-06-06 00:25:35 +0300

    Added bad credentials exception handling

--- AuthorizationServer/src/main/java/net/quazar/authorizationserver/exception/AuthorizationServerExceptionHandler.java
+++ AuthorizationServer/src/main/java/net/quazar/authorizationserver/exception/AuthorizationServerExceptionHandler.java
@@ -4,6 +4,7 @@ import net.quazar.authorizationserver.entity.AuthServerError;
 import org.springframework.core.annotation.AnnotationUtils;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -23,6 +24,16 @@ public class AuthorizationServerExceptionHandler {
                 .body(AuthServerError.builder()
                         .status(status.value())
                         .message(e.getMessage())
+                        .timestamp(LocalDateTime.now())
+                        .build());
+    }
+
+    @ExceptionHandler(BadCredentialsException.class)
+    public ResponseEntity<AuthServerError> handle(BadCredentialsException e) {
+        return ResponseEntity.status(403)
+                .body(AuthServerError.builder()
+                        .status(403)
+                        .message("Неверный логин или пароль")
                         .timestamp(LocalDateTime.now())
                         .build());
     }
