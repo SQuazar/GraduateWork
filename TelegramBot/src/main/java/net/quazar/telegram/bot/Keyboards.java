1e7dd94e2650284f63f884c34d2c5770a27a8a0b
SQuazar <flawesv@yandex.ru>
2023-06-05 04:02:38 +0300

    Removed most keyboards

--- TelegramBot/src/main/java/net/quazar/telegram/bot/Keyboards.java
+++ TelegramBot/src/main/java/net/quazar/telegram/bot/Keyboards.java
@@ -2,10 +2,8 @@ package net.quazar.telegram.bot;
 
 import net.quazar.telegram.bot.commands.TextCommands;
 import net.quazar.telegram.bot.handler.StateHandler;
-import org.telegram.telegrambots.meta.api.objects.replykeyboard.InlineKeyboardMarkup;
 import org.telegram.telegrambots.meta.api.objects.replykeyboard.ReplyKeyboard;
 import org.telegram.telegrambots.meta.api.objects.replykeyboard.ReplyKeyboardMarkup;
-import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.InlineKeyboardButton;
 import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.KeyboardButton;
 import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.KeyboardRow;
 
@@ -36,58 +34,9 @@ public final class Keyboards {
                 .build();
     }
 
-    public static ReplyKeyboard mySubscriptionKeyboard() {
-        return ReplyKeyboardMarkup.builder()
-                .keyboard(List.of(
-                        new KeyboardRow(List.of(
-                                KeyboardButton.builder()
-                                        .text(TextCommands.MySubscriptionCommands.CATEGORIES)
-                                        .build()
-                        )),
-                        new KeyboardRow(List.of(
-                                KeyboardButton.builder()
-                                        .text(TextCommands.MySubscriptionCommands.UNSUBSCRIBE)
-                                        .build()
-                        )),
-                        new KeyboardRow(List.of(
-                                KeyboardButton.builder()
-                                        .text(TextCommands.BACK_TO_MENU)
-                                        .build()
-                        ))
-                ))
-                .resizeKeyboard(true)
-                .build();
-    }
-
-    public static ReplyKeyboard categoriesEdit() {
-        return ReplyKeyboardMarkup.builder()
-                .keyboard(List.of(
-                        new KeyboardRow(List.of(
-                                KeyboardButton.builder()
-                                        .text(TextCommands.MyCategoriesCommands.SUBSCRIBE_CATEGORY)
-                                        .build()
-                        )),
-                        new KeyboardRow(List.of(
-                                KeyboardButton.builder()
-                                        .text(TextCommands.MyCategoriesCommands.UNSUBSCRIBE_CATEGORY)
-                                        .build()
-                        )),
-                        new KeyboardRow(List.of(
-                                KeyboardButton.builder()
-                                        .text(TextCommands.MyCategoriesCommands.GO_BACK)
-                                        .build()
-                        ))
-                ))
-                .resizeKeyboard(true)
-                .build();
-    }
-
     public static ReplyKeyboard getKeyboardByState(int state) {
         return switch (state) {
             case StateHandler.State.START -> startKeyboard();
-            case StateHandler.State.MY_SUBSCRIPTION -> mySubscriptionKeyboard();
-            case StateHandler.State.CATEGORIES_EDIT, StateHandler.State.CATEGORY_SUBSCRIBE, StateHandler.State.CATEGORY_UNSUBSCRIBE ->
-                    categoriesEdit();
             default -> menuKeyboard();
         };
     }
