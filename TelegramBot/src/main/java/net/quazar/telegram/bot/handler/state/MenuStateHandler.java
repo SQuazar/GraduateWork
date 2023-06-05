8b86bddeaad6015882a1cb970e528ca7fa5f9a06
SQuazar <flawesv@yandex.ru>
2023-06-05 04:02:54 +0300

    DI changed

--- TelegramBot/src/main/java/net/quazar/telegram/bot/handler/state/MenuStateHandler.java
+++ TelegramBot/src/main/java/net/quazar/telegram/bot/handler/state/MenuStateHandler.java
@@ -4,7 +4,7 @@ import net.quazar.telegram.bot.Keyboards;
 import net.quazar.telegram.bot.commands.MySubscriptionCommand;
 import net.quazar.telegram.bot.commands.TextCommands;
 import net.quazar.telegram.bot.handler.StateHandler;
-import net.quazar.telegram.proxy.ResourceServerProxy;
+import net.quazar.telegram.bot.service.SubscriptionService;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Message;
 import org.telegram.telegrambots.meta.api.objects.Update;
@@ -17,15 +17,15 @@ import java.time.format.DateTimeFormatter;
 public class MenuStateHandler implements StateHandler {
     private final MySubscriptionCommand mySubscriptionCommand;
 
-    public MenuStateHandler(ResourceServerProxy resourceServerProxy, DateTimeFormatter dateTimeFormatter) {
-        this.mySubscriptionCommand = new MySubscriptionCommand(resourceServerProxy, dateTimeFormatter);
+    public MenuStateHandler(SubscriptionService subscriptionService, DateTimeFormatter dateTimeFormatter) {
+        this.mySubscriptionCommand = new MySubscriptionCommand(subscriptionService, dateTimeFormatter);
     }
 
     @Override
     public int handle(Update update, AbsSender absSender) throws TelegramApiException {
         Message message = update.getMessage();
         if (message.getText().equals(TextCommands.MenuCommands.MY_SUBSCRIPTION))
-            return mySubscriptionCommand.execute(update, absSender);
+            return mySubscriptionCommand.execute(update, update.getMessage().getFrom().getId(), absSender);
         SendMessage sendMessage = SendMessage.builder()
                 .chatId(message.getFrom().getId())
                 .text("Команда не найдена")
